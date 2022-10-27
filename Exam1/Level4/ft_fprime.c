64d6728c62a43bb391bb9d4d6145bc3873dd164c
Cristian <104097821+crist6494@users.noreply.github.com>
2022-10-27 20:31:28 +0200

    Update ft_fprime.c

--- Exam1/Level4/ft_fprime.c
+++ Exam1/Level4/ft_fprime.c
@@ -19,34 +19,34 @@ La entrada, cuando la haya, será válida. */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-void	fprime(unsigned int num)
+void	fprime(int n)
 {
-	unsigned int prime;
-	
-	if (num == 1)
-		printf("1");
-	else
-	{
-		prime = 2;
-		while (num > 1)
+	int i = 2;
+	if (n ==1)
+			printf("1");
+		while(i <= n)
 		{
-			if (num % prime == 0)
+			if (n % i == 0)
 			{
-				printf ("%d", prime);
-				num = num / prime;
-				if (num > 1)
-					printf("*");
-				prime--;
+				printf("%d", i);
+				if (n == i)
+					break;
+				printf("*");
+				n = n / i;
+				i = 2;
 			}
-			prime++;
+			i++;
 		}
-	}
 }
-int main (int argc, char **argv)
-{
+
+
+int main(int argc, char **argv)
+{	
 	if (argc == 2)
 		fprime(atoi(argv[1]));
-	printf ("\n");
+	printf("\n");
 	return (0);
+}return (0);
 }
